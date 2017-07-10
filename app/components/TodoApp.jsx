29e29ba5ac28894b753c9f6622eeabebdd2411a6
mticciati <matthewticciati@gmail.com>
2017-07-09 21:59:15 -0700

    sf load in AddTodo instead of AddTodoForm

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,7 +3,7 @@ import uuid from 'uuid';
 import moment from 'moment';
 
 import VisibleItemList from '../containers/VisibleItemList';
-import AddTodoForm from 'AddTodoForm';
+import AddTodo from '../containers/AddTodo';
 import SearchTodos from 'SearchTodos';
 import TodoAPI from 'TodoAPI';
 
@@ -12,7 +12,7 @@ class TodoApp extends React.Component {
   constructor(props) {
     super(props);
 
-    this.handleAddTodo = this.handleAddTodo.bind(this);
+    // this.handleAddTodo = this.handleAddTodo.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.state = {
       todos: TodoAPI.getTodos(),
@@ -29,20 +29,20 @@ class TodoApp extends React.Component {
     this.setState(data);
   }
 
-  handleAddTodo(text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(), 
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    });
-  }
+  // handleAddTodo(text) {
+  //   this.setState({
+  //     todos: [
+  //       ...this.state.todos,
+  //       {
+  //         id: uuid(), 
+  //         text: text,
+  //         completed: false,
+  //         createdAt: moment().unix(),
+  //         completedAt: undefined
+  //       }
+  //     ]
+  //   });
+  // }
 
   render() {
     let {todos, showCompleted, searchText} = this.state;
@@ -55,7 +55,7 @@ class TodoApp extends React.Component {
             <div className="container">
               <SearchTodos onSearch={this.handleSearch} />
               <VisibleItemList />
-              <AddTodoForm onAddTodo={this.handleAddTodo} />
+              <AddTodo />
             </div>
           </div>
         </div>
